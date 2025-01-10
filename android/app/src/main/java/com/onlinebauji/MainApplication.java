37a6a9d1ab4c2d38e5659660bab2f534cb28acfc
Mohd Areeb Safvi <47021490+Areeb0206@users.noreply.github.com>
2025-01-10 13:17:13 +0530

    lol

--- android/app/src/main/java/com/onlinebauji/MainApplication.java
+++ android/app/src/main/java/com/onlinebauji/MainApplication.java
@@ -39,9 +39,9 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
-                new MainReactPackage(),
-                new RNGestureHandlerPackage(),
-                new ReanimatedPackage()
+                // new MainReactPackage(),
+                // new RNGestureHandlerPackage(),
+                // new ReanimatedPackage()
             );
         }
 
